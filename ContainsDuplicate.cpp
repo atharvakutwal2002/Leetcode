52848d573b27f7cf26981e98139e2845abec87f2
Atharva <atharvakutwal2002@gmail.com>
2022-01-27 14:22:15 +0530

    19th commit (contains duplicate in array)

---
+++ ContainsDuplicate.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+bool containsDuplicate(vector<int> &nums)
+{
+    if (nums.size()==1)
+    {
+        return false;
+    }
+    
+    sort(nums.begin(), nums.end());
+    for (int i = 0; i < nums.size()-1; i++)
+    {
+        if (nums[i] == nums[i + 1])
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+    return false;
+}
+
+int main(){
+    vector<int> v{1,1,1,3,3,4,3,2,4,2};
+    cout<<containsDuplicate(v);
+
+    return 0;
+}
