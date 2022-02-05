f970d008ada5f4ec40ad1524205d335fea5eda2f
Atharva <atharvakutwal2002@gmail.com>
2022-02-05 09:16:44 +0530

    majority element added

--- majority_element.cpp
+++ majority_element.cpp
@@ -1,8 +1,30 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int majorityElement(vector<int> &nums)
-{
-    int start=0;
+// int majorityElement(vector<int> &nums)
+// {
+//     int n=nums.size();
+//     int count =0;
+//     int temp=0;
+//     sort(nums.begin(),nums.end());
+//     for (int i = 0; i < n; i++)
+//     {
+//         if (nums[i]==nums[i+1])
+//         {
+//             temp++;
+//             if (temp> count)
+//             {
+//                 count=temp;
+//             }
+//             return count;
+//         }
+//         temp=0;
+//     }
+//     return count;
     
+// }
+
+int majorityElement(vector<int> &nums){
+    sort(nums.begin(),nums.end());
+    return nums[(int)nums.size()/2];
 }
