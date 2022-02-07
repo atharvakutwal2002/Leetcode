c27ef2da7ad1d604e732931078bd1a125e6ec503
Atharva <atharvakutwal2002@gmail.com>
2022-02-07 10:03:16 +0530

    max product of two numbers added

---
+++ max_product_of_two_numbers.cpp
@@ -0,0 +1,13 @@
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    int maxProduct(vector<int> &nums)
+    {
+        sort(nums.begin(), nums.end());
+        return (nums[nums.size() - 1] - 1) * (nums[nums.size() - 2] - 1);
+    }
+};
