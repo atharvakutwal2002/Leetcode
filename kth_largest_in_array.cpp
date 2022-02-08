935a681152d53ea682466df1b02d43b0f8e87c0d
Atharva <atharvakutwal2002@gmail.com>
2022-02-08 16:00:21 +0530

    kth largest element in array added

---
+++ kth_largest_in_array.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    int findKthLargest(vector<int> &nums, int k)
+    {
+        sort(nums.begin(), nums.end());
+        return nums[nums.size() - k];
+    }
+};
