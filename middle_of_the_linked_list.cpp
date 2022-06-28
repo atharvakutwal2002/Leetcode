40e336c51582e1290ef6b964d09e802cf6bf765e
Atharva <atharvakutwal2002@gmail.com>
2022-06-28 18:05:46 +0530

    middle of linked list

--- middle_of_the_linked_list.cpp
+++ middle_of_the_linked_list.cpp
@@ -1,13 +1,48 @@
-#include<bits/stdc++.h>
-using namespace std;
+// #include<bits/stdc++.h>
+// using namespace std;
 
-ListNode* middleNode(ListNode* head) {
-    ListNode * slow = head;
-    ListNode * fast= head;
-    while (fast != NULL)
-    {
-        slow= slow->next;
-        fast = fast->next->next;
+// ListNode* middleNode(ListNode* head) {
+//     int count=0;
+//     ListNode * slow = head;
+//     ListNode * fast= head;
+//     while (fast && fast->next)
+//     {
+//         fast = fast->next->next;
+//         count+=2;
+//         slow= slow->next;
+//     }
+//     if (count%2==0)
+//     {
+//         return slow->next;
+//     }
+    
+//      return slow; 
+// }
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* middleNode(ListNode* head) {
+         int count=0;
+        ListNode * slow = head;
+        ListNode * fast= head;
+        while (fast && fast->next)
+        {
+            fast = fast->next->next;
+            
+            slow= slow->next;
+        }
+        
+        return slow;
+          
     }
-     return slow; 
-}
+};
