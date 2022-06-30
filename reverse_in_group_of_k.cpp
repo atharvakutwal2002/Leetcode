f8e1a7893525b79fe2090acaa1a5b3920e7eb2a2
Atharva <atharvakutwal2002@gmail.com>
2022-06-30 17:49:45 +0530

    correct solution of reverse in group of k

--- reverse_in_group_of_k.cpp
+++ reverse_in_group_of_k.cpp
@@ -1,5 +1,83 @@
-#include <bits/stdc++.h>
-using namespace std;
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// // ListNode *reverseKGroup(ListNode *head, int k)
+// // {
+// //     if (head == NULL)
+// //     {
+// //         return NULL;
+// //     }
+// //     int size = 0;
+// //     ListNode *te mp = head;
+// //     while (temp != NULL)
+// //     {
+// //         temp = temp->next;
+// //         size++;
+// //     }
+// //     int remainder = size/ k;
+// //     if (size % k == 0)
+// //     {
+// //         int count = 0;
+// //         ListNode *prev = NULL;
+// //         ListNode *curr = head ;
+// //         ListNode *forward = NULL;
+
+// //         while (curr != NULL && count < k)
+// //         {
+// //             forward = curr->next;
+// //             curr->next = prev;
+
+// //             prev = curr;
+// //             curr = forward;
+// //             count++;
+// //         }
+// //         if (forward != NULL)
+// //         {
+// //             head->next = reverseKGroup(forward, k);
+// //         }
+
+// //         return prev;
+// //     }
+
+// //         int count = 0;
+// //         ListNode *prev = NULL;
+// //         ListNode *curr = head;
+// //         ListNode *forward = NULL;
+
+// //         while (curr != NULL && count < remainder)
+// //         {
+// //             forward = curr->next;
+// //             curr->next = prev;
+
+// //             prev = curr;
+// //             curr = forward;
+// //             count++;
+// //         }
+// //         if (forward != NULL)
+// //         {
+// //             head->next = reverseKGroup(forward, k);
+// //         }
+
+// //         return prev;
+
+// // }
+
+// int remaining_nodes(ListNode *head)
+// {
+//     if (head == NULL)
+//     {
+//         return 0;
+//     }
+
+//     int cnt = 0;
+//     ListNode *temp = head;
+//     while (temp != NULL)
+//     {
+//         temp = temp->next;
+//         cnt++;
+//     }
+//     return cnt;
+// }
 
 // ListNode *reverseKGroup(ListNode *head, int k)
 // {
@@ -7,62 +85,91 @@ using namespace std;
 //     {
 //         return NULL;
 //     }
-//     int size = 0;
-//     ListNode *te mp = head;
-//     while (temp != NULL)
+
+//     int count = 0;
+//     ListNode *prev = NULL;
+//     ListNode *curr = head;
+//     ListNode *forward = NULL;
+
+//     while (curr != NULL && count < k)
 //     {
-//         temp = temp->next;
-//         size++;
+//         forward = curr->next;
+//         curr->next = prev;
+
+//         prev = curr;
+//         curr = forward;
+//         count++;
 //     }
-//     int remainder = size/ k;
-//     if (size % k == 0)
+//     if (forward != NULL && remaining_nodes(head->next) >= k)
 //     {
-//         int count = 0;
-//         ListNode *prev = NULL;
-//         ListNode *curr = head ;
-//         ListNode *forward = NULL;
-
-//         while (curr != NULL && count < k)
-//         {
-//             forward = curr->next;
-//             curr->next = prev;
-
-//             prev = curr;
-//             curr = forward;
-//             count++;
-//         }
-//         if (forward != NULL)
-//         {
-//             head->next = reverseKGroup(forward, k);
-//         }
-
-//         return prev;
+//         head->next = reverseKGroup(forward, k);
 //     }
 
-//         int count = 0;
-//         ListNode *prev = NULL;
-//         ListNode *curr = head;
-//         ListNode *forward = NULL;
-
-//         while (curr != NULL && count < remainder)
-//         {
-//             forward = curr->next;
-//             curr->next = prev;
-
-//             prev = curr;
-//             curr = forward;
-//             count++;
-//         }
-//         if (forward != NULL)
-//         {
-//             head->next = reverseKGroup(forward, k);
-//         }
-
-//         return prev;
-
+//     return prev;
 // }
 
-ListNode *reverseKGroup(ListNode *head, int k)
+// ********   final correct solution   **********  //
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution
 {
-    
-}
+public:
+    int remaining_nodes(ListNode *head)
+    {
+        if (head == NULL)
+        {
+            return 0;
+        }
+
+        int cnt = 0;
+        ListNode *temp = head;
+        while (temp != NULL)
+        {
+            temp = temp->next;
+            cnt++;
+        }
+
+        return cnt;
+    }
+    ListNode *reverseKGroup(ListNode *head, int k)
+    {
+        if (head == NULL)
+        {
+            return NULL;
+        }
+
+        int count = 0;
+        ListNode *prev = NULL;
+        ListNode *curr = head;
+        ListNode *forward = NULL;
+
+        while (curr != NULL && count < k)
+        {
+            forward = curr->next;
+            curr->next = prev;
+
+            prev = curr;
+            curr = forward;
+            count++;
+        }
+        if (forward != NULL && remaining_nodes(forward) >= k)
+        {
+            head->next = reverseKGroup(forward, k);
+        }
+        else
+        {
+            head->next = forward;
+        }
+
+        return prev;
+    }
+};
